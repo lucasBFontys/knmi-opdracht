9ca16cb6fa7dfe8a91ed1518a49235062809e788
Lucas <lucasbitalia@gmail.com>
2025-03-18 11:02:12 +0100

    contact fonts opgesteld en tailwind config

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,52 +1,52 @@
 export default function Contact() {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-wit">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+                <h2 className="mb-4 text-4xl tracking-tight font-heading font-bold text-center text-black">
                     Neem contact op
                 </h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-                    Heb je een technisch probleem? Wil je feedback geven over een bètaversie? Meer weten over ons Business-plan? Laat het ons weten.
+                <p className="mb-8 lg:mb-16 font-body text-center text-pretty text-black sm:text-xl">
+                    Heb je een technisch probleem? Wil je feedback geven over de app? Meer weten over ons de KNMI? Laat het ons weten.
                 </p>
                 <form action="#" className="space-y-8">
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        <label htmlFor="email" className="block mb-2 text-sm font-body font-medium text-gray-900">
                             Jouw e-mail
                         </label>
                         <input
                             type="email"
                             id="email"
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 font-accent  font-extralight text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                             placeholder="naam@voorbeeld.com"
                             required
                         />
                     </div>
                     <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        <label htmlFor="subject" className="block mb-2 text-sm font-body font-medium text-gray-900">
                             Onderwerp
                         </label>
                         <input
                             type="text"
                             id="subject"
-                            className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            className="block p-3 w-full text-sm font-accent font-extralight text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                             placeholder="Laat ons weten hoe we je kunnen helpen"
                             required
                         />
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                        <label htmlFor="message" className="block mb-2 text-sm font-body font-medium text-gray-900">
                             Jouw bericht
                         </label>
                         <textarea
                             id="message"
                             rows="6"
-                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            className="block p-2.5 w-full text-sm font-accent font-extralight text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                             placeholder="Laat een bericht achter..."
                         ></textarea>
                     </div>
                     <button
                         type="submit"
-                        className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        className="py-3 px-5 text-sm font-body text-center text-black rounded-lg bg-groen sm:w-fit hover:bg-groendark focus:ring-4 focus:outline-none focus:ring-primary-300"
                     >
                         Bericht versturen
                     </button>
