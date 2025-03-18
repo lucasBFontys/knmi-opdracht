6a7c95071c6d888c9c885e4283916dcebac6aa61
Lucas <lucasbitalia@gmail.com>
2025-03-18 15:41:55 +0100

    footer V1 component toegevoegd

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,17 @@
+import Footer from "./components/footer/footer.jsx";
 import "./globals.css";
+
+export const metadata = {
+  title: "KNMI Weer App",
+  description: "De KNMI Weer App is de beste weer app van Nederland.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
+        <Footer />
       </body>
     </html>
   );
