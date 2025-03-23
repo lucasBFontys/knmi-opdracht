334b4bc320921236013b1c44cf54d0d856ef75ef
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-03-23 16:17:05 +0100

    mockup toegevoegd aan page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+import Mockup from "./components/mockup/mockup.jsx"
 import Reviews from "./components/reviews/reviews.jsx";
+
 import "./globals.css";
 
 export default function Home() {
   return (
     <main>
+      <Mockup/>
       <Reviews />
+      
     </main>
   );
 }
