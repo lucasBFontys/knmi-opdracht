5800b5eaa1ed165dbc949d863af7422d4870e724
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-03-23 15:56:37 +0100

    tekst, buttons en mockup toegevoegd

--- app/components/mockup/mockup.jsx
+++ app/components/mockup/mockup.jsx
@@ -4,6 +4,57 @@ export default function Page() {
   return (
       <section>
           
+          <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 text-center">
+              
+<h1 class="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Met de KNMI-app ben je voorbereid op gevaarlijk weer</h1>
+<p class="mb-10 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Ontvang realtime waarschuwingen voor stormen, hevige regenval of andere extreme weersomstandigheden, zodat je veilig kunt handelen. Of je nu thuis bent of onderweg, de app helpt je voorbereid te zijn op wat het weer ook brengt.</p>
+
+<a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-groen rounded-[29.50px] mr-4">
+
+    <svg class=" w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"></svg>
+    
+                  Download</a>
+              <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-groen rounded-[29.50px]">
+
+    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"></svg>
+    
+                  Download</a>
+              
+{/* mockup */}
+          </div>
+          <div class="flex items-center justify-center">
+
+    <div class="relative w-72 h-[600px] rounded-[45px] shadow-[0_0_2px_2px_rgba(255,255,255,0.1)] border-8 border-zinc-900">
+
+        <div class="absolute top-2 left-1/2 transform -translate-x-1/2 w-[90px] h-[22px] bg-zinc-900 rounded-full z-20">
+        </div>
+
+        <div class="absolute -inset-[1px] border-[3px] border-zinc-700 border-opacity-40 rounded-[37px] pointer-events-none"></div>
+
+        
+    
+        <div class="relative w-full h-full  rounded-[37px] overflow-hidden flex items-center justify-center  bg-zinc-900/10">
+            <svg class="text-zinc-700 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M17.05 20.28c-.98.95-2.05.8-3.08.35c-1.09-.46-2.09-.48-3.24 0c-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8c1.18-.24 2.31-.93 3.57-.84c1.51.12 2.65.72 3.4 1.8c-3.12 1.87-2.38 5.98.48 7.13c-.57 1.5-1.31 2.99-2.54 4.09zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25c.29 2.58-2.34 4.5-3.74 4.25" />
+            </svg>
+            <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-24 w-12 bg-zinc-600 blur-[80px]">
+            </div>
+        </div>
+        
+    
+        <div class="absolute left-[-12px] top-20 w-[6px] h-8 bg-zinc-900 rounded-l-md shadow-md"></div>
+        
+      
+        <div class="absolute left-[-12px] top-36 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
+        
+
+        <div class="absolute left-[-12px] top-52 w-[6px] h-12 bg-zinc-900 rounded-l-md shadow-md"></div>
+        
+      
+        <div class="absolute right-[-12px] top-36 w-[6px] h-16 bg-zinc-900 rounded-r-md shadow-md"></div>
+    </div>
+</div>
+          
   </section>
   )
 }
