9b2037dd795762c4d11d7be522d818110545e02b
Ouassila00 <145725226+Ouassila00@users.noreply.github.com>
2025-03-23 16:17:30 +0100

    /nieuws gewijzigd naar /Nieuws

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -100,7 +100,7 @@ export default function Page() {
                             </li>
                             <li>
                                 <Link
-                                    href="/nieuws"
+                                    href="/Nieuws"
                                     className="block py-2 pr-4 pl-3 text-blauw border-b hover:underline lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:"
                                 >
                                     Nieuws & Updates
