110121143eb3a7a5ebe376618088d1c77bd301e2
SemThe <485391@student.fontys>
2025-03-27 23:11:30 +0100

    Emoticons toegevoegd
    Emoticons toegevoegd aan de tekst op de home pagina

--- app/components/features/page.jsx
+++ app/components/features/page.jsx
@@ -38,21 +38,21 @@ export default function Features() {
     <div className="w-full px-6 pt-24 bg-[var(--color-offwit)] text-left font-body">
       <div className="max-w-2xl mx-auto">
         <AnimatedParagraph>
-          Weerapps zijn er genoeg. Maar hoe vaak open je er één en word je overspoeld met advertenties,
+          Weerapps zijn er genoeg. Maar hoe vaak open je er één en word je overspoeld met advertenties 📢,
           vage voorspellingen en overbodige extra’s?
         </AnimatedParagraph>
         <AnimatedParagraph>Dat kan anders.</AnimatedParagraph>
         <AnimatedParagraph>
-          De KNMI-app is er voor iedereen. Gemaakt voor het volk,
-          zonder advertenties, zonder ruis. Gewoon betrouwbaar weer, direct van de bron.
+          De KNMI-app is er voor iedereen. Gemaakt voor het volk 👥,
+          zonder advertenties 🚫📢, zonder ruis. Gewoon betrouwbaar weer 🌤️, direct van de bron.
         </AnimatedParagraph>
         <AnimatedParagraph>
           Met een unieke functie die écht telt: officiële weerswaarschuwingen
-          bij gevaarlijk weer. Storm, gladheid, extreme hitte—je weet het als eerste.
+          bij gevaarlijk weer. Storm ⛈️, gladheid ❄️, extreme hitte ☀️je weet het als eerste.
         </AnimatedParagraph>
         <div className="flex items-center">
           <AnimatedParagraph>
-            Duidelijk, snel, betrouwbaar. Zodat jij altijd voorbereid bent.
+            Duidelijk, snel, betrouwbaar ✅. Zodat jij altijd voorbereid bent 🎯.
           </AnimatedParagraph>
         </div>
       </div>
