0296f236ad947428af8e470e7508979b5ecd5cbe
SemThe <485391@student.fontys>
2025-03-27 09:53:11 +0100

    Animatie verbeterd
    Animatie smoother gemaakt. Ook toegevoegd dat de tekst weer grijs word als het buiten beeld valt.

--- app/components/features/page.jsx
+++ app/components/features/page.jsx
@@ -5,30 +5,36 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 export default function Features() {
-  // Custom component voor geanimeerde paragraaf
   function AnimatedParagraph({ children }) {
     const controls = useAnimation();
     const { ref, inView } = useInView({
-      triggerOnce: true,  // Dit zorgt ervoor dat de animatie maar 1 keer wordt uitgevoerd
-      rootMargin: "-25% 0px", // De trigger wordt geactiveerd wanneer de paragraaf in het midden van het scherm is
+      triggerOnce: false,
+      threshold: 0.9,
     });
 
     useEffect(() => {
       if (inView) {
         controls.start({ color: "#000000", transition: { duration: 0.6 } });
+      } else {
+        controls.start({ color: "#9CA3AF", transition: { duration: 0.6 } });
       }
     }, [controls, inView]);
 
     return (
-      <motion.p ref={ref} animate={controls} initial={{ color: "#9CA3AF" }} className="mb-6 text-1xl md:text-2xl font-semibold">
+      <motion.p
+        ref={ref}
+        animate={controls}
+        initial={{ color: "#F1F6FD" }}
+        className="mb-6 text-lg md:text-xl font-semibold"
+      >
         {children}
       </motion.p>
     );
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6 py-12 bg-[var(--color-offwit)] text-left">
-      <div className="max-w-2xl text-lg md:text-xl">
+    <div className="w-full px-6 py-12 bg-[var(--color-offwit)] text-left">
+      <div className="max-w-2xl mx-auto">
         <AnimatedParagraph>
           Weerapps zijn er genoeg. Maar hoe vaak open je er één en word je overspoeld met advertenties, 
           vage voorspellingen en overbodige extra’s?
